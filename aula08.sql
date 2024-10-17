24febaf4f53420dd327e44e5aaf4f09d093e6bd8
labsfiap <labsfiap@PA406MICRO100.fiap.com.br>
2024-10-17 11:54:22 -0300

    second commit

---
+++ aula08.sql
@@ -0,0 +1,106 @@
+-- APELIDOS
+
+-- falha de ambiguidade, pois há duas colunas com o mesmo nome 
+-- entre as tabelas
+SELECT nome
+FROM Pokemons p, Tipos t; 
+
+-- utilizando apelidos para facilitar
+SELECT p.nome
+FROM Pokemons p, Tipos t; 
+
+-- quando não utilizamos apelidos é necessário mencionar o nome da tabela 
+SELECT Pokemons.nome
+FROM Pokemons, Tipos; 
+
+-- definindo apelidos nas colunas quando as mesmas tem o mesmo nome
+
+SELECT p.nome, t.nome
+FROM Pokemons p, Tipos t; 
+
+-- exemplo de definição de apelidos em colunas
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p, Tipos t; 
+
+
+
+-- JUNÇÃO
+
+-- equi join
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p, Tipos t
+WHERE t.tipoid = p.tipoid; 
+
+-- inner join
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p INNER JOIN Tipos t ON t.tipoid = p.tipoid; 
+
+
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p, Tipos t
+WHERE t.tipoid = p.tipoid AND t.nome='Fogo'; 
+
+
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p INNER JOIN Tipos t ON t.tipoid = p.tipoid
+WHERE t.nome='Fogo'; 
+
+
+-- LEFT JOIN
+
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p LEFT JOIN Tipos t ON t.tipoid = p.tipoid;
+
+
+-- RIGHT JOIN
+
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p RIGHT JOIN Tipos t ON t.tipoid = p.tipoid;
+
+
+-- LEFT EXCLUDING JOIN
+
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p LEFT JOIN Tipos t ON t.tipoid = p.tipoid
+WHERE T.tipoid is null;
+
+
+-- RIGHT EXCLUDING JOIN
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p RIGHT JOIN Tipos t ON t.tipoid = p.tipoid
+WHERE P.tipoid IS NULL;
+
+-- FULL JOIN
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p FULL JOIN Tipos t ON t.tipoid = p.tipoid;
+
+
+-- OU
+-- o unnion é utilizar para juntar duas ou mais consultas
+-- o full join é LEFT JOIN + RIGHT JOIN
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p LEFT JOIN Tipos t ON t.tipoid = p.tipoid
+UNION
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p RIGHT JOIN Tipos t ON t.tipoid = p.tipoid;
+
+-- OUTER JOIN OU EXCLUDING JOIN 
+-- o OUTER join é LEFT EXLCUDING JOIN + RIGHT EXLCUDING JOIN
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p LEFT JOIN Tipos t ON t.tipoid = p.tipoid
+WHERE T.tipoid is null
+UNION
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p RIGHT JOIN Tipos t ON t.tipoid = p.tipoid
+WHERE P.tipoid IS NULL;
+
+-- CROSS JOIN
+SELECT p.nome nome_pokemon, t.nome tipo
+FROM Pokemons p, Tipos t; 
+
+
+--SELF JOIN
+
+SELECT p1.pokemonid, p1.nome nome_pokemon, p2.pokemonid, p2.nome nome_pokemon
+FROM Pokemons p1 INNER JOIN Pokemons p2 ON p1.tipoid = p2.tipoid
+WHERE p1.pokemonid< p2.pokemonid; 
